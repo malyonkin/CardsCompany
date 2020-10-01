71c2552391018bc1d65bc45548b3a416de1ecb43
malyonkin <malyonkin.a@gmail.com>
2020-10-01 10:16:47 +0300

    add test java filse

---
+++ src/main/java/app/forTest.java
@@ -0,0 +1,13 @@
+package app;
+
+public class forTest {
+
+    public static void main(String[] args) {
+        int[] a = new int[] {4,5,6,7,8};
+        int c = a.length;
+        int[] b = new int[] {a.length};
+        for (int i=0;i<6;++i) {
+            System.out.println(c);
+        }
+    }
+}

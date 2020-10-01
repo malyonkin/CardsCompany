98d1898495f3ce1830d2ae837e9e42bbc11848e2
malyonkin <malyonkin.a@gmail.com>
2020-10-01 11:35:27 +0300

    updated txt files

--- src/main/java/app/Cat.java
+++ src/main/java/app/Cat.java
@@ -6,9 +6,9 @@ public class Cat {
     int age;
 
     public static void cats() {
-        Cat barsik = new Cat();
-        barsik.age = 3;
-        barsik.name = "Барсик";
+        Cat barsik = new Cat(); //создание объекта класса
+        barsik.age = 2;
+        barsik.name = "Барсик1";
 
         System.out.println("Мы создали кота по имени " + barsik.name + ", его возраст - " + barsik.age);
     }

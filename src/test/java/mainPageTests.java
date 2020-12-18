58b5abb6831b3e9479d13dda16deb2b7383178d0
malyonkin <malyonkin.a@gmail.com>
2020-12-18 10:24:47 +0300

    1) allure + junit описание аннотаций

--- src/test/java/mainPageTests.java
+++ src/test/java/mainPageTests.java
@@ -2,17 +2,13 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.*;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -24,7 +20,7 @@ public class mainPageTests {
     //mvn allure:serve - Report will be generated into temp folder. Web server with results will start.
     //mvn allure:report - Report will be generated tо directory: target/site/allure-maven/index.html
 
-    @BeforeAll
+    @BeforeAll //Sonar https://automation-remarks.com/vyvieditie-vashi-tiesty-na-novyi-urovien/index.html
     static void setup() { //ВАЖНО: Для Allure необходимо настроить сравнение результатов с историей - Trend - https://github.com/fescobar/allure-docker-service#keep-history-and-trends
         //настройки браузера и тд
         //работа с БД - подготовка тестовых данных, наполнение или очистка данных
@@ -35,24 +31,20 @@ public class mainPageTests {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
     }
 
+    //для Allure
     @Severity(value = SeverityLevel.TRIVIAL)
     @Issue(value = "RBCNEW-123") //указывается ID дефекта в баг-треккинговой системе
     @TmsLinks({@TmsLink(value = "X-ray-135"), @TmsLink(value = "X-ray-136")}) //Позволяет линковать автотесты с тестовыми кейсами, шаги которых описаны в системах управления тестированием
-    @Step("СТЕП Алюр - Добавить ссылку на система Карточки компаний") //пока непонятно, как работает
+    //@Step("СТЕП Алюр - Добавить ссылку на система Карточки компаний") //Прикручиваются к методам для page object (дир main) - для логирования в Allure
     @Description(value = "ОПИСАНИЕ Алюр - описание в сьюте")
     @Epic(value = "ЭПИК Алюр - Функционала Тест. Позволяет группировать тесты по эпикам") //https://habr.com/ru/company/sberbank/blog/359302/  и https://docs.qameta.io/allure/
     @Feature(value = "ФИЧА Алюр - для тестирования структуры Тестовой модели")
-    @Test
     @Flaky //нотификация для нестабильных тестов
+    //для jUnit5
+    @Tag("smoke")
     @DisplayName("Для тестирования")
-    public void addLinkSber() {
-        String link = "https://www.rbc.ru/companies/";
-        Allure.addAttachment("Результат", "text/plain", link);
-    }
-
     @Test
-    @DisplayName("удалить")
-    public void test() {
+    public void addLinkSber() {
         String link = "https://www.rbc.ru/companies/";
         Allure.addAttachment("Результат", "text/plain", link);
     }

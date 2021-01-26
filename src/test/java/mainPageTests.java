6d1b868168c6a8313db7167b9a8ab07e8a49119b
malyonkin <malyonkin.a@gmail.com>
2021-01-26 12:43:16 +0300

    1) добавлены тестовые атрибуты

--- src/test/java/mainPageTests.java
+++ src/test/java/mainPageTests.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.by;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
@@ -25,6 +26,7 @@ public class mainPageTests {
         //настройки браузера и тд
         //работа с БД - подготовка тестовых данных, наполнение или очистка данных
     }
+
     //@Before
     @BeforeEach
     public void setUp() {
@@ -34,10 +36,12 @@ public class mainPageTests {
     //для Allure
     @Severity(value = SeverityLevel.TRIVIAL)
     @Issue(value = "RBCNEW-123") //указывается ID дефекта в баг-треккинговой системе
-    @TmsLinks({@TmsLink(value = "X-ray-135"), @TmsLink(value = "X-ray-136")}) //Позволяет линковать автотесты с тестовыми кейсами, шаги которых описаны в системах управления тестированием
+    @TmsLinks({@TmsLink(value = "X-ray-135"), @TmsLink(value = "X-ray-136")})
+    //Позволяет линковать автотесты с тестовыми кейсами, шаги которых описаны в системах управления тестированием
     //@Step("СТЕП Алюр - Добавить ссылку на система Карточки компаний") //Прикручиваются к методам для page object (дир main) - для логирования в Allure
     @Description(value = "ОПИСАНИЕ Алюр - описание в сьюте")
-    @Epic(value = "ЭПИК Алюр - Функционала Тест. Позволяет группировать тесты по эпикам") //https://habr.com/ru/company/sberbank/blog/359302/  и https://docs.qameta.io/allure/
+    @Epic(value = "ЭПИК Алюр - Функционала Тест. Позволяет группировать тесты по эпикам")
+    //https://habr.com/ru/company/sberbank/blog/359302/  и https://docs.qameta.io/allure/
     @Feature(value = "ФИЧА Алюр - для тестирования структуры Тестовой модели")
     @Flaky //нотификация для нестабильных тестов
     //для jUnit5
@@ -52,7 +56,8 @@ public class mainPageTests {
     //@Test
     @Owner(value = "Малёныч Сашкин")
     @Severity(value = SeverityLevel.BLOCKER) //SeverityLevel: BLOCKER, CRITICAL, NORMAL, MINOR или TRIVIAL
-    @Epic(value = "ЭПИК Алюр - Функционала поиска. Позволяет группировать тесты по эпикам") //https://habr.com/ru/company/sberbank/blog/359302/  и https://docs.qameta.io/allure/
+    @Epic(value = "ЭПИК Алюр - Функционала поиска. Позволяет группировать тесты по эпикам")
+    //https://habr.com/ru/company/sberbank/blog/359302/  и https://docs.qameta.io/allure/
     @Feature(value = "ФИЧА Алюр - пустой поиск. Часть функционала ПОИСКа")
     @Test
     @DisplayName("Проверка полного поиска - значение НУЛЕВОЕ/Не задано")
@@ -62,7 +67,16 @@ public class mainPageTests {
         getWebDriver().findElement(By.cssSelector(".home__search-form .search-form__btn")).click(); //использование selenium в selenide
         Thread.sleep(1000);
         $("h3").shouldHave(Condition.text("Все результаты"))
-                            .shouldBe(exist)
-                            .shouldBe(visible);
-        }
+                .shouldBe(exist)
+                .shouldBe(visible);
+    }
+
+    //Вводим тестовые атрибуты для WEB-разработчиков. Ниже хорошие примеры с тестовыми атрибутыми, которые использует facebook
+    @Test
+    void trestAttributeLogin() throws InterruptedException {
+        open("https://ru-ru.facebook.com/");
+        $("[data-testid='royal_email']").val("testE-mail");
+        $(by("data-testid", "royal_pass")).val("testPassword");
+        Thread.sleep(3000);
+    }
 }
